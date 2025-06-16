5f2ae42800a96e7db41e6d585c998d07024202f7
Steven Falcones León <140020193+fsteven96@users.noreply.github.com>
2025-06-16 12:44:03 -0500

    Update Conexion.cs

--- PeluqueriaAnita/Datos/Conexion.cs
+++ PeluqueriaAnita/Datos/Conexion.cs
@@ -4,7 +4,7 @@ namespace PeluqueriaAnita.Datos
 {
     public class Conexion
     {
-        private const string Conex = "Server=DESKTOP-L1PP6AB\\SQLEXPRESS;Database=PeluqueriaAnita;Trusted_Connection=True;";
+        private const string Conex = "Server=tcp:servidorsteven.database.windows.net,1433;Initial Catalog=PeluqueriaAnita;Persist Security Info=False;User ID=adminSteven;Password=Soydecristo1@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         public SqlConnection GetConnection()
         {
             return new SqlConnection(Conex);
