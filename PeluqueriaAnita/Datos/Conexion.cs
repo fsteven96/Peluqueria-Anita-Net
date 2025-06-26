53d55066cd288fb0642f58ebe36fb4b0053cb0f0
Steven Falcones León <140020193+fsteven96@users.noreply.github.com>
2025-06-26 00:22:09 -0500

    Primer commit del proyecto Peluquería Anita

--- PeluqueriaAnita/Datos/Conexion.cs
+++ PeluqueriaAnita/Datos/Conexion.cs
@@ -4,7 +4,7 @@ namespace PeluqueriaAnita.Datos
 {
     public class Conexion
     {
-        private const string Conex = "Server=tcp:servidorsteven.database.windows.net,1433;Initial Catalog=PeluqueriaAnita;Persist Security Info=False;User ID=adminSteven;Password=Soydecristo1@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private const string Conex = "Server=tcp:servidorsteven.database.windows.net,1433;Initial Catalog=PeluqueriaAnita;Persist Security Info=False;User ID=;Password=;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         public SqlConnection GetConnection()
         {
             return new SqlConnection(Conex);
