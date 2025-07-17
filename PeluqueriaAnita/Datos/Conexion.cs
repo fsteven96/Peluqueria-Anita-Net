bd5924351abbc806ecdf467e4c87c4cc755a75e5
Steven Falcones León <140020193+fsteven96@users.noreply.github.com>
2025-07-17 16:21:33 -0500

    Update Conexion.cs

--- PeluqueriaAnita/Datos/Conexion.cs
+++ PeluqueriaAnita/Datos/Conexion.cs
@@ -4,7 +4,7 @@ namespace PeluqueriaAnita.Datos
 {
     public class Conexion
     {
-        private const string Conex = "Server=jsfl.database.windows.net,1433;Initial Catalog=PeluqueriaAnita;Persist Security Info=False;User ID=publico;Password=Soydecristo1@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private const string Conex = "Server=tcp:jsfl.database.windows.net,1433;Initial Catalog=PeluqueriaAnita;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication="Active Directory Default";";
         public SqlConnection GetConnection()
         {
             return new SqlConnection(Conex);
